b4f3702f0579d5e811a8dbdb95fb4e4849f04a93
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-13 23:31:12 -0400

    reformat

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
-	export let name;
+    export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>Hello {name}!</h1>
+    <p>
+        Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+        how to build Svelte apps.
+    </p>
 </main>
 
 <style>
