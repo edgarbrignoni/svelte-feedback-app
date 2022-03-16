b67e5dd3b1e29c8233e586f67895c0cf28437faa
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-16 10:07:38 -0400

    add info and style to FeedbackItem component

--- src/components/FeedbackItem.svelte
+++ src/components/FeedbackItem.svelte
@@ -3,4 +3,37 @@
     export let item;
 </script>
 
-<Card>Hello</Card>
+<Card>
+    <div class="num-display">
+        {item.rating}
+    </div>
+    <div class="close">X</div>
+    <p class="text-display">
+        {item.text}
+    </p>
+</Card>
+
+<style>
+    .num-display {
+        position: absolute;
+        top: -10px;
+        left: -10px;
+        width: 50px;
+        height: 50px;
+        background: #ff6a95;
+        color: #fff;
+        border: 1px #eee solid;
+        border-radius: 50%;
+        padding: 10px;
+        text-align: center;
+        font-size: 19px;
+    }
+    .close {
+        position: absolute;
+        top: 10px;
+        right: 20px;
+        cursor: pointer;
+        background: none;
+        border: none;
+    }
+</style>
