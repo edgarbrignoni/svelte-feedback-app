66a3ccbce741cf76788c7113d0c04cb4c24971fc
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-16 00:27:23 -0400

    creating a for loop and like in react when you loop thru data you need to create a key in this caser we use user.id

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,20 @@
     let lastName = "value";
     let color = "blue";
     let showText = false;
+    let users = [
+        {
+            id: 1,
+            name: "Alexa",
+        },
+        {
+            id: 2,
+            name: "Siri",
+        },
+        {
+            id: 3,
+            name: "Computer",
+        },
+    ];
 
     $: name = firstName + " " + lastName;
 
@@ -23,6 +37,9 @@
         <p>Click the button to change color back and re-display text.</p>
     {/if}
     <button on:click={toggle}>Change color</button>
+    {#each users as user (user.id)}
+        <h3>{user.id}: {user.name}</h3>
+    {/each}
 </main>
 
 <style>
