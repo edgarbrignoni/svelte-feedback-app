19d0ba4820d3d26042902ce718e903b166f7b8b8
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-16 00:49:04 -0400

    add feedback items

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,21 @@
 <script>
+    let feedback = [
+        {
+            id: 1,
+            rating: 10,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+        },
+        {
+            id: 2,
+            rating: 9,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+        },
+        {
+            id: 3,
+            rating: 8,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+        },
+    ];
 </script>
 
 <main>
