bc30703c30a5901ed64f421f3c4490282af1eb4d
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-16 09:59:13 -0400

    pass the props to FeedbackItem component

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,9 +1,11 @@
 <script>
+    import FeedbackItem from "./FeedbackItem.svelte";
+
     export let feedback = [];
 
     console.log(feedback);
 </script>
 
 {#each feedback as fb (fb.id)}
-    <h3>{fb.text}</h3>
+    <FeedbackItem item={fb} />
 {/each}
