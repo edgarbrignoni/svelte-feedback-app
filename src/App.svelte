5ee23dc9628e6a6425eb1ff81ef6973a0f7d7492
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-19 17:17:37 -0400

    create stores. we can create stores as a folder and add multiple stores, but the project is simple enough to add just a js file on the root of teh project. we also move the feedback data to the stores js

--- src/App.svelte
+++ src/App.svelte
@@ -2,23 +2,6 @@
     import FeedbackForm from "./components/FeedbackForm.svelte";
     import FeedbackList from "./components/FeedbackList.svelte";
     import FeedbackStats from "./components/FeedbackStats.svelte";
-    let feedback = [
-        {
-            id: 1,
-            rating: 10,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-        },
-        {
-            id: 2,
-            rating: 9,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-        },
-        {
-            id: 3,
-            rating: 8,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-        },
-    ];
 
     $: count = feedback.length;
     $: average =
