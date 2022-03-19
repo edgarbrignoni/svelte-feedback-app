60aa79ac33cbe3fa346e941cc91150640ea4fcc3
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-19 12:50:56 -0400

    pass the event from the FeedbackItem to FeedbackList via custom event delete-feedback

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,10 +1,8 @@
 <script>
     import FeedbackItem from "./FeedbackItem.svelte";
     export let feedback = [];
-
-    console.log(feedback);
 </script>
 
 {#each feedback as fb (fb.id)}
-    <FeedbackItem item={fb} />
+    <FeedbackItem item={fb} on:delete-feedback />
 {/each}
