47757c574791b897ff3b371dff18c897a47860ad
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-19 13:14:44 -0400

    create form

---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<form action="">
+    <!-- Rating Select -->
+    <div class="input-group">
+        <input
+            type="text"
+            placeholder="Tell us something that keeps you coming back"
+        />
+        <button>Send</button>
+    </div>
+</form>
