4528aa6df4180405539b03dd38b6bd5e76105805
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-19 18:12:30 -0400

    so now we can make it to add feedback no to the array on the main component but directly in the form via FeedbackStore

--- src/App.svelte
+++ src/App.svelte
@@ -2,34 +2,10 @@
     import FeedbackForm from "./components/FeedbackForm.svelte";
     import FeedbackList from "./components/FeedbackList.svelte";
     import FeedbackStats from "./components/FeedbackStats.svelte";
-
-    let feedback = [
-        {
-            id: 1,
-            rating: 10,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-        },
-        {
-            id: 2,
-            rating: 9,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-        },
-        {
-            id: 3,
-            rating: 8,
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-        },
-    ];
-
-    const addFeedback = (e) => {
-        const newFeedback = e.detail;
-        feedback = [newFeedback, ...feedback];
-        console.log("feedback", feedback);
-    };
 </script>
 
 <main class="container">
-    <FeedbackForm on:add-feedback={addFeedback} />
+    <FeedbackForm />
     <FeedbackStats />
     <FeedbackList />
 </main>
