224d246b546fadc74824b713d704bc7250e3ac93
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-19 17:13:15 -0400

    adding cool transitions to the FeedbackList. You can not added to a custom component, you need to wrap it on a div, you can apply multiples transition to the same div like scale and fade

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,8 +1,11 @@
 <script>
+    import { fade, scale } from "svelte/transition";
     import FeedbackItem from "./FeedbackItem.svelte";
     export let feedback = [];
 </script>
 
 {#each feedback as fb (fb.id)}
-    <FeedbackItem item={fb} on:delete-feedback />
+    <div in:scale out:fade={{ duration: 500 }}>
+        <FeedbackItem item={fb} on:delete-feedback />
+    </div>
 {/each}
