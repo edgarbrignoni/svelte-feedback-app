d324112d6006635fee8a13ba5ef85cc304252150
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-19 17:49:24 -0400

    there is an easier way by using the FeedbackStore directly so it automatically unsubscribe it will still work in a short hand way

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,23 +1,10 @@
 <script>
-    import { onMount, onDestroy } from "svelte";
     import { FeedbackStore } from "../stores";
     import { fade, scale } from "svelte/transition";
     import FeedbackItem from "./FeedbackItem.svelte";
-    let feedback = [];
-
-    const unsubscibe = FeedbackStore.subscribe((data) => {
-        console.log("data", data);
-        feedback = data;
-    });
-
-    onMount(() => {
-        console.log("mounted");
-    });
-
-    onDestroy(() => {});
 </script>
 
-{#each feedback as fb (fb.id)}
+{#each $FeedbackStore as fb (fb.id)}
     <div in:scale out:fade={{ duration: 500 }}>
         <FeedbackItem item={fb} on:delete-feedback />
     </div>
