c8ad254e50e24fb504809e0f683be80c4893f43d
Edgar Brignoni <edgarbrignoni@icloud.com>
2022-03-19 13:47:16 -0400

    create a Button component, style it and add to the form. This button is going to be disable until the condition of minimum characters is met like a real time validation and a message is going to be display conditionally

--- src/components/FeedbackForm.svelte
+++ src/components/FeedbackForm.svelte
@@ -1,13 +1,72 @@
 <script>
+    import Card from "./Card.svelte";
+    import Button from "./Button.svelte";
+    import { trusted } from "svelte/internal";
+
+    let text = "";
+    let btnDisable = true;
+    let min = 10;
+    let message;
+
+    const handleInput = () => {
+        if (text.trim().length < min - 1) {
+            message = `Text must be at least ${min} characters`;
+            btnDisable = true;
+        } else {
+            message = null;
+            btnDisable = false;
+        }
+    };
 </script>
 
-<form action="">
-    <!-- Rating Select -->
-    <div class="input-group">
-        <input
-            type="text"
-            placeholder="Tell us something that keeps you coming back"
-        />
-        <button>Send</button>
-    </div>
-</form>
+<Card>
+    <header><h2>How would you rate your service with us?</h2></header>
+    <form action="">
+        <!-- Rating Select -->
+        <div class="input-group">
+            <input
+                type="text"
+                on:input={handleInput}
+                bind:value={text}
+                placeholder="Tell us something that keeps you coming back"
+            />
+            <Button disabled={btnDisable} type="submit">Send</Button>
+        </div>
+        {#if message}
+            <div class="message">{message}</div>
+        {/if}
+    </form>
+</Card>
+
+<style>
+    header {
+        max-width: 400px;
+        margin: auto;
+    }
+    header h2 {
+        font-size: 22px;
+        font-weight: 600;
+        text-align: center;
+    }
+    .input-group {
+        display: flex;
+        flex-direction: row;
+        border: 1px solid #ccc;
+        padding: 8px 10px;
+        border-radius: 8px;
+        margin-top: 15px;
+    }
+    input {
+        flex-grow: 2;
+        border: none;
+        font-size: 16px;
+    }
+    input:focus {
+        outline: none;
+    }
+    .message {
+        padding-top: 10px;
+        text-align: center;
+        color: rebeccapurple;
+    }
+</style>
